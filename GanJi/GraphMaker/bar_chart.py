38613a0273fdc50359a65162c423cdf418b2cd3f
= <=>
2017-09-20 13:53:04 +0800

    武汉赶集网二手条目

---
+++ GanJi/GraphMaker/bar_chart.py
@@ -0,0 +1,59 @@
+#统计自己爬的各个地区总帖子数前20名柱状图
+import pymongo, charts
+
+conn = pymongo.MongoClient('localhost', 27017)
+ganji_db = conn['ganji']
+data_collection = ganji_db['data']
+
+#清洗地区数据,只保留地级市
+for i in data_collection.find():
+    data_collection.update({'_id':i['_id']}, {'$set':{'area':i['area'].split('-')[0]}})
+
+
+########以下部分可以使用pipline轻松解决,速度更快
+citys = list(set([i['area'] for i in data_collection.find()]))
+#找到所有的城市
+citys_data = [(i,data_collection.find({'area':i}).count()) for i in citys]
+#构造各个城市的对应发帖数量的元组列表
+citys_data = sorted(citys_data, key=lambda x:x[1], reverse=True)
+#将其排序
+#######
+
+
+#进行柱状图展示前十名的城市
+series = [{
+    'name':i[0],
+    'data':[i[1]],
+    'type':'column',
+} for i in citys_data[:20] ]
+
+#charts.plot(series, show='inline')
+charts.plot(series, show='inline', options=dict(title=dict(text='武汉赶集网帖子发布地区分布前10')))
+
+
+
+for i in data_collection.find({'area':'北京'}, {'cates':1, '_id':0, 'pub_date':1}).limit(300):
+    print(i)
+    #find的参数:第一个字典是查找,第二个是显示的数据项,0代表不显示
+
+#手动输入效果
+# series = [
+#     {
+#     'name': 'OS X',
+#     'data': [11],
+#     'type': 'column'
+# }, {
+#     'name': 'Ubuntu',
+#     'data': [8],
+#     'type': 'column',
+#     'color':'#ff0066'
+# }, {
+#     'name': 'Windows',
+#     'data': [12],
+#     'type': 'column'
+# }, {
+#     'name': 'Others',
+#     'data': [29],
+#     'type': 'column'
+# }
+#          ]
---
+++ GanJi/main.py
@@ -0,0 +1,92 @@
+from bs4 import BeautifulSoup
+from multiprocessing import Pool
+import requests, pymongo,time, random, threading
+
+##从http://cn-proxy.com/获取几个较好的代理切换使用
+proxy_list = [
+    '101.200.45.131:3128',
+    '121.40.199.105:80',
+    '120.25.211.80:9999',
+    '114.215.102.168:8081'
+]
+
+proxies_ip = random.choice(proxy_list)
+proxies = {'http' : proxies_ip}
+
+headers = {
+    'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36',
+    'Connection':'keep-active'
+}
+
+
+#获取所有二手商品频道
+def get_channels():
+    url_host = 'http://wh.ganji.com'
+    web_data = requests.get(url_host + '/wu', headers=headers, proxies=proxies)
+    soup = BeautifulSoup(web_data.text, 'lxml')
+    channels = [url_host + i.get('href') for i in soup.select('#wrapper div.main dd a')]
+    return channels
+
+#获取某个二手商品频道的某个页面下的所有url
+def get_pg_urls(url):
+    #time.sleep(random.randint(1,3))
+    try:
+        web_data = requests.get(url, headers=headers, proxies=proxies)
+        soup = BeautifulSoup(web_data.text, 'lxml')
+        if len(soup.select('div.noinfo')) != 0:
+            print('fail to get this page...')
+            pass       #页数超过, 没有商品信息
+        else:
+            all_tags = soup.select('td.t a.t')
+            tags = filter(lambda x: len(x.find_all('span')) == 0, all_tags) #筛去带有"精"标签的广告帖
+            urls = [i.get('href').split('?')[0] for i in tags]
+            for i in urls:
+                print(i)
+                url_collection.insert_one({'url' : i})
+    except:
+        print('爬取页面信息失败')
+
+#获取某个频道下所有的url, 默认最多为70页
+def get_channel_urls(channel_url, pg_num=70):
+    for i in range(1, pg_num + 1):
+        # time.sleep(random.randint(1, 3))
+        get_pg_urls(channel_url + 'o' + str(i))
+    print('done the channel:%s' % (channel_url,))
+
+#获取某个详情帖子的信息
+def get_detail_data(url):
+    try:
+        web_data = requests.get(url,  headers=headers, proxies=proxies)
+        soup = BeautifulSoup(web_data.text, 'lxml')
+
+        title = soup.select('h1.info_titile')[0].get_text()
+        price = soup.select('span.price_now > i')[0].get_text()
+        area = soup.select('div.palce_li i')[0].get_text()
+        views = int(soup.select('span.look_time')[0].get_text()[:-3])
+
+        data_collection.insert_one({
+            'title':title,
+            'price':price,
+            'area':area,
+            'views':views
+        })
+    except:
+        print('获取详情数据失败')
+
+
+conn = pymongo.MongoClient('localhost', 27017)
+ganji_db = conn['ganji']
+url_collection = ganji_db['url']
+data_collection = ganji_db['data']
+
+if __name__ == '__main__':
+    url_pool = Pool()
+    data_pool = Pool()
+
+    channels = get_channels()
+    print(channels)
+    url_pool.map(get_channel_urls, channels)    #先获取所有的详情页url
+
+    data_pool.map(get_detail_data, [i['url'] for i in url_collection.find()])
+    #根据获取的url逐一获取信息
+
